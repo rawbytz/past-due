87f7e32bf512f5194b9d2c84a981eda765266e90
rawbytz <rawbytz@gmail.com>
2020-03-04 13:24:12 -0700

    first release

--- pastDue.js
+++ pastDue.js
@@ -1,5 +1,5 @@
 (function pastDue_0_1() {
   const roundToNearestHour = date => new Date(Math.round(date.getTime() / 36e5) * 36e5); // fix for WorkFlowy search bug
-  const end = roundToNearestHour(new Date()).toLocaleDateString("en-US", { hour: 'numeric' });
-  WF.search(`1/1/1970 - ${end}`);
+  const nowish = roundToNearestHour(new Date()).toLocaleDateString("en-US", { hour: 'numeric' });
+  WF.search(`1/1/1970 - ${nowish}`);
 })();
